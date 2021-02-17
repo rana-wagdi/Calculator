286257e417caa9dac3a67c229018736fde505498
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-02-17 14:15:39 -0800

    req,res with html files

--- Calculator.js
+++ Calculator.js
@@ -2,8 +2,8 @@ const express = require("express");
 
 const app = express();
 
-app.get("/",function(request, response){
-    response.send("Hello World");
+app.get("/", function(request, response){
+    response.sendFile(__dirname + "/index.html")
 })
 
 app.listen(3000,function(){
