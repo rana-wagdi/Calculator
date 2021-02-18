04f9e2798fb63d7843a4bc2ada3351e4385c7e0f
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-02-17 18:37:46 -0800

    bmi routing

--- Calculator.js
+++ Calculator.js
@@ -16,6 +16,19 @@ app.post("/", function(req, res){
 
         res.send("The result of the calculation is " + result);
 })
+app.get("/bmicalculator", function (req, res) {
+  res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmicalculator", function(req, res){
+
+    var weight = parseFloat(req.body.weight);
+    var height = parseFloat(req.body.height);
+
+    var bmi = weight / (height * height) ;
+
+    res.send("Your BMI is "+ bmi);
+});
 
 app.listen(3000,function(){
     console.log("server is running in a port 3000");
